7be199fa14a0f47d894fa967c346e65ed6226539
dmv287 <dvasquezmontes@simonsfoundation.org>
2020-09-18 20:01:25 -0400

    ini

---
+++ Main.py
@@ -0,0 +1,9 @@
+# # # Objetivos:
+# # # Obtener data de MyAnimeList sobre anime de este año
+# - - - - - - - - - - Imports - - - - - - - - - -
+import re, time
+import datetime as dt
+import pandas as pd
+from bs4 import BeautifulSoup
+from webbot import Browser
+
